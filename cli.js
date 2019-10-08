a7e6683c4541819950fcbdedd47abb3c75118394
Shastel <max@shas.tel>
2019-10-08 16:35:25 +0300

    🗃 Add output to .json

--- cli.js
+++ cli.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+
 const commandLineArgs = require('command-line-args');
 
 const loadData = require('./lib');
 
-const { path, repo, limit, message, ext = [], ignoreExt } = commandLineArgs([
+const { path, repo, limit, message, ext = [], ignoreExt, output } = commandLineArgs([
   { name: 'repo', type: String, alias: 'r', defaultOption: process.cwd, description: 'test' },
   { name: 'limit', type: Number, alias: 'l', defaultOption: Infinity },
   { name: 'path', type: String, alias: 'p', defaultOption: '/' },
   { name: 'message', type: String, alias: 'm', defaultOption: '' },
-  { name: 'ext', type: String, alias: 'e', multiple: true,  }
+  { name: 'ext', type: String, alias: 'e', multiple: true,  },
+  { name: 'output', type: String, alias: 'o'  }
 ]);
 
 (async () => {
@@ -22,6 +25,17 @@ const { path, repo, limit, message, ext = [], ignoreExt } = commandLineArgs([
     ignoreExt,
   });
 
-  entries.forEach(entry => console.log(entry.join(' => ')));
+  if (!output) {
+    // eslint-disable-next-line
+    entries.forEach(entry => console.log(entry.join(' => ')));
+    return ;
+  }
 
+  const entriesAsObject = entries.reduce((R, [ fileName, occurrences ]) => {
+    R[fileName] = occurrences;
+
+    return R;
+  }, {})
+
+  fs.writeFileSync(output, JSON.stringify(entriesAsObject));
 })();
